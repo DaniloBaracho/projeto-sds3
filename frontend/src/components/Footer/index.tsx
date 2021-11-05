110260271c66a7e54257b5f804184e3480a96fb4
DaniloBaracho <danilobaracho@gmail.com>
2021-11-05 17:48:14 -0300

    Layers

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="linkedin.com/in/danilo-baracho-nunes-41aa9313b" target="_blank" rel="noreferrer">Danilo Baracho Nunes</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer> 
+    );
+}
+
+export default Footer;
