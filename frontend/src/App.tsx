7c2880ad255257f3e0b8eb9fd82e55610be98ea4
DaniloBaracho <danilobaracho@gmail.com>
2021-11-06 21:44:26 -0300

    Group by search

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import NavBar from 'components/NavBar';
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dash Board Vendas</h1>
+        <DataTable />
+      </div>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+          <BarChart />
+        </div>
+
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas Vendas</h5>
+          <DonutChart />
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
